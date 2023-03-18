63e9c3b8d943d88a7d5e997a4463491698be0f69
Ana Muraya <Anamuraya9@gmail.com>
2023-03-18 22:31:58 +0300

    new solution

--- script.js
+++ script.js
@@ -1,27 +1,9 @@
-function arraysEqual(a, b) {
-  if (a === b) return true;
-  if (a == null || b == null) return false;
-  if (a.length !== b.length) return false;
-  return a.every((val, idx) => val === b[idx]);
-}
-
 function solution(a, b) {
-  if (arraysEqual(a, b)) return true;
-
-  for (let i = 0; i < a.length; i++) {
-    if (!b.includes(a[i])) return false;
-    const element = a[i];
-
-    for (let j = 0; j < a.length; j++) {
-      let arr = [...a];
-      arr.splice(j, 1, element);
-      arr.splice(i, 1, a[j]);
-
-      if (arraysEqual(arr, b)) return true;
-    }
-  }
-
-  return false;
+  const newa = a.filter((x, i) => x != b[i]);
+  const newb = b.filter((x, i) => x != a[i]);
+  return (
+    newa.length == 0 || (newa.length == 2 && newa.join("") == newb.rexerse().join(""))
+  );
 }
 
 console.log(solution(([1, 2, 3], [1, 2, 3]))); //true
